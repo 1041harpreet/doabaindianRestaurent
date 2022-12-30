d3bfd19c2087786f556c0da01aa37ed9c23da0aa
harpreet singh <1041harpreet@gmail.com>
2022-12-30 16:13:30 +0530

    admin updated

---
+++ lib/admin/widgets/drawer.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+
+import '../../config/config.dart';
+
+Widget drawer(context,authprovider){
+ return  ListView(
+    // Important: Remove any padding from the ListView.
+    padding: EdgeInsets.zero,
+    children: [
+       UserAccountsDrawerHeader( // <-- SEE HERE
+        decoration: BoxDecoration(color:AppConfig.primaryColor),
+        accountName: Text(
+          authprovider.username,
+          style: AppConfig.blacktext
+        ),
+
+        accountEmail: Text(
+          authprovider.user.email,
+          style: AppConfig.blacktext,
+          ),
+        currentAccountPicture: Image.asset('assets/images/avatar.png'),
+      ),
+      ListTile(
+        leading: const Icon(
+          Icons.home,
+          color: Colors.black,
+        ),
+        title:  Text('Gallery',style: AppConfig.blacktext),
+        onTap: () {
+          Navigator.pop(context);
+        },
+      ),
+      ListTile(
+        leading:  const Icon(
+          Icons.bookmark_border,
+          color: Colors.black,
+        ),
+        title:  Text('All Orders',style: AppConfig.blacktext),
+        onTap: () {
+          Navigator.pop(context);
+        },
+      ),
+    ],
+  );
+}
