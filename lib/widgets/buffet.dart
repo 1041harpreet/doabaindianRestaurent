709ac0ad533f20bdbab7ab4d87a3c77de893a958
harpreet singh <1041harpreet@gmail.com>
2022-12-30 16:13:30 +0530

    admin updated

---
+++ lib/widgets/buffet.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+Widget buffet(){
+  return Container(
+    // height: 500.0,
+      padding: const EdgeInsets.all(8.0),
+      child: Image.asset('assets/images/doaba.jpg')
+  );
+}
